74e5bddf92a0241c87fd7e35aa7fe317e24726d9
Ludwig Seitz <ludwig@sics.se>
2017-04-07 16:05:05 +0200

    Stub files for OSCOAP profile

---
+++ src/main/java/se/sics/ace/coap/as/OscoapAS.java
@@ -0,0 +1,9 @@
+package se.sics.ace.coap.as;
+
+public class OscoapAS {
+
+    public OscoapAS() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ src/main/java/se/sics/ace/coap/rs/OscoapProfile.java
@@ -0,0 +1,9 @@
+package se.sics.ace.coap.rs;
+
+public class OscoapProfile {
+
+    public OscoapProfile() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
