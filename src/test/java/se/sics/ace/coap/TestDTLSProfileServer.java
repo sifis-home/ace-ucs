5e441a87dbd22fee0d56c317326c5afcf47fb429
Ludwig Seitz <ludwig@sics.se>
2017-04-25 16:07:15 +0200

    Implemented server for testing DTLSProfileDeliverer

--- src/test/java/se/sics/ace/coap/TestDTLSProfileServer.java
+++ src/test/java/se/sics/ace/coap/TestDTLSProfileServer.java
@@ -31,88 +31,78 @@
  *******************************************************************************/
 package se.sics.ace.coap;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
 
-import org.bouncycastle.crypto.InvalidCipherTextException;
-import org.eclipse.californium.core.coap.CoAP.Code;
-import org.eclipse.californium.core.coap.CoAP.Type;
-import org.eclipse.californium.core.coap.CoAP;
-import org.eclipse.californium.core.coap.Request;
-import org.eclipse.californium.core.network.CoapEndpoint;
-import org.eclipse.californium.core.network.Exchange;
-import org.eclipse.californium.core.network.Exchange.Origin;
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.CoapServer;
 import org.eclipse.californium.core.server.resources.CoapExchange;
-import org.eclipse.californium.scandium.ScandiumLogger;
-import org.eclipse.californium.scandium.auth.PreSharedKeyIdentity;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.eclipse.californium.core.server.resources.Resource;
 
 import com.upokecenter.cbor.CBORObject;
 
 import COSE.AlgorithmID;
-import COSE.CoseException;
 import COSE.KeyKeys;
 import COSE.MessageTag;
 import COSE.OneKey;
-import se.sics.ace.AceException;
+
 import se.sics.ace.COSEparams;
 import se.sics.ace.Constants;
-import se.sics.ace.coap.rs.DTLSProfileAuthzInfo;
+import se.sics.ace.coap.rs.DTLSProfileAsInfo;
+import se.sics.ace.coap.rs.DTLSProfileDeliverer;
 import se.sics.ace.coap.rs.DTLSProfileTokenRepository;
-import se.sics.ace.cwt.CWT;
 import se.sics.ace.cwt.CwtCryptoCtx;
-import se.sics.ace.examples.KissTime;
 import se.sics.ace.examples.KissValidator;
-import se.sics.ace.rs.AuthzInfo;
-import se.sics.ace.rs.TokenRepository;
 
 /**
- * Test the DTLSProfileAuthzInfo class.
+ * Server for testing the DTLSProfileDeliverer class. 
+ * 
+ * The Junit tests are in TestDTLSProfileDeliverer, but you MUST
+ * run this server first for the tests to worl.
  * 
  * @author Ludwig Seitz
  *
  */
-public class TestDTLSProfileAuthzInfo {
+public class TestDTLSProfileServer {
 
-    static {
-        ScandiumLogger.initialize();
-        ScandiumLogger.setLevel(Level.FINE);
+
+    /**
+     * Definition of the Hello-World Resource
+     */
+    public static class HelloWorldResource extends CoapResource {
+        
+        /**
+         * Constructor
+         */
+        public HelloWorldResource() {
+            
+            // set resource identifier
+            super("helloWorld");
+            
+            // set display name
+            getAttributes().setTitle("Hello-World Resource");
+        }
+
+        @Override
+        public void handleGET(CoapExchange exchange) {
+            
+            // respond to the request
+            exchange.respond("Hello World!");
+        }
     }
     
-    private static byte[] key128a = {'c', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    private static byte[] key128 = {'a', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    private static DTLSProfileTokenRepository tr;
-    private static CwtCryptoCtx ctx;
-    private static AuthzInfo ai;
-    private static DTLSProfileAuthzInfo dai;
-    private static CBORObject payload;
     
     /**
-     * Set up the necessary objects.
-     * 
-     * @throws CoseException
-     * @throws AceException
-     * @throws IOException
-     * @throws InvalidCipherTextException 
-     * @throws IllegalStateException 
+     * The CoAPs server for testing, run this before running the Junit tests.
+     *  
+     * @param args
+     * @throws Exception
      */
-    @BeforeClass
-    public static void setUp() 
-            throws CoseException, AceException, IOException, 
-            IllegalStateException, InvalidCipherTextException {
-        
-        //Set up DTLSProfileTokenRepository
+    public static void main(String[] args) throws Exception {
+      //Set up DTLSProfileTokenRepository
         Set<String> actions = new HashSet<>();
         actions.add("GET");
         Map<String, Set<String>> myResource = new HashMap<>();
@@ -132,21 +122,21 @@ public class TestDTLSProfileAuthzInfo {
         
         DTLSProfileTokenRepository.create(
                 valid, "src/test/resources/tokens.json", null);
-        tr = DTLSProfileTokenRepository.getInstance();
+        DTLSProfileTokenRepository tr 
+            = DTLSProfileTokenRepository.getInstance();
+       
+        byte[] key128 
+            = {'a', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        
+        byte[] key128a 
+            = {'c', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+      
         
         //Set up COSE parameters
         COSEparams coseP = new COSEparams(MessageTag.Encrypt0, 
                 AlgorithmID.AES_CCM_16_128_128, AlgorithmID.Direct);
-        ctx = CwtCryptoCtx.encrypt0(key128a, coseP.getAlg().AsCBOR());
-        
-        //Set up the inner Authz-Info library
-        ai = new AuthzInfo(tr, Collections.singletonList("TestAS"), 
-                new KissTime(), 
-                null,
-                valid, ctx);
-        
-        //Set up the DTLS authz-info resource
-        dai = new DTLSProfileAuthzInfo(ai);
+        CwtCryptoCtx ctx 
+            = CwtCryptoCtx.encrypt0(key128a, coseP.getAlg().AsCBOR());
         
         //Set up a token to use
         Map<String, CBORObject> params = new HashMap<>(); 
@@ -163,51 +153,23 @@ public class TestDTLSProfileAuthzInfo {
         key.add(KeyKeys.KeyId, kid);
         key.add(KeyKeys.Octet_K, CBORObject.FromObject(key128));
         params.put("cnf", key.AsCBOR());
-        CWT token = new CWT(params);
-        payload = token.encode(ctx);
+        tr.addToken(params, ctx);
         
+        DTLSProfileAsInfo ai 
+            = new DTLSProfileAsInfo("coaps://blah/authz-info/");
+        DTLSProfileDeliverer dpd 
+            = new DTLSProfileDeliverer(new HelloWorldResource(), tr, null, ai); 
         
+        CoapServer server = new CoapServer();
+        
+        Resource resource = new HelloWorldResource();
+        
+        server.add(resource);
+        server.setMessageDeliverer(dpd);
+        server.start();
+        System.out.println("Server starting");
     }
     
-    /**
-     * Test a POST to /authz-info
-     * @throws UnknownHostException 
-     * @throws AceException 
-     */
-    @Test
-    public void testPOSTtoken() throws UnknownHostException, AceException {
-        Request req = new Request(Code.POST);
-        req.setPayload(payload.EncodeToBytes());
-        req.setDestination(InetAddress.getLocalHost());
-        req.setSenderIdentity(new PreSharedKeyIdentity("psk"));
-        req.setType(Type.NON);
-        req.setAcknowledged(false);
-        req.setSource(InetAddress.getLocalHost());
-        req.setSourcePort(CoAP.DEFAULT_COAP_PORT);
-        req.setToken(new byte[]{0x01});
-        Exchange iex = new Exchange(req, Origin.REMOTE);
-        iex.setRequest(req);       
-        iex.setEndpoint(new CoapEndpoint());
-        CoapExchange ex = new CoapExchange(iex, dai);      
-        dai.handlePOST(ex);
-        
-        //Test that the PoP key was stored
-        Assert.assertArrayEquals(key128,
-                ai.getKey("ourKey").get(KeyKeys.Octet_K).GetByteString());
-        
-        
-       //Test that the token is there
-        Assert.assertEquals(TokenRepository.OK, 
-                tr.canAccess("ourKey", "ourKey", "temp", "GET", 
-                        new KissTime(), null));
-    }
-    
-    
-    /**
-     * Deletes the test file after the tests
-     */
-    @AfterClass
-    public static void tearDown() {
-        new File("src/test/resources/tokens.json").delete();
-    }
+
+
 }
