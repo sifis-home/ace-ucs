6d1aeeb51b010bde9544c6ac7126d11504d9a0a7
Ludwig Seitz <ludwig@sics.se>
2017-04-25 14:14:24 +0200

    Implemented more tests on DTLSProfileAuthzInfo

--- src/test/java/se/sics/ace/coap/TestDTLSProfileAuthzInfo.java
+++ src/test/java/se/sics/ace/coap/TestDTLSProfileAuthzInfo.java
@@ -45,13 +45,16 @@ import java.util.logging.Level;
 import org.bouncycastle.crypto.InvalidCipherTextException;
 import org.eclipse.californium.core.coap.CoAP.Code;
 import org.eclipse.californium.core.coap.CoAP.Type;
+import org.eclipse.californium.core.coap.CoAP;
 import org.eclipse.californium.core.coap.Request;
+import org.eclipse.californium.core.network.CoapEndpoint;
 import org.eclipse.californium.core.network.Exchange;
 import org.eclipse.californium.core.network.Exchange.Origin;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 import org.eclipse.californium.scandium.ScandiumLogger;
 import org.eclipse.californium.scandium.auth.PreSharedKeyIdentity;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -72,6 +75,7 @@ import se.sics.ace.cwt.CwtCryptoCtx;
 import se.sics.ace.examples.KissTime;
 import se.sics.ace.examples.KissValidator;
 import se.sics.ace.rs.AuthzInfo;
+import se.sics.ace.rs.TokenRepository;
 
 /**
  * Test the DTLSProfileAuthzInfo class.
@@ -86,6 +90,7 @@ public class TestDTLSProfileAuthzInfo {
         ScandiumLogger.setLevel(Level.FINE);
     }
     
+    private static byte[] key128a = {'c', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     private static byte[] key128 = {'a', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     private static OneKey symmetricKey;
     private static DTLSProfileTokenRepository tr;
@@ -113,7 +118,7 @@ public class TestDTLSProfileAuthzInfo {
         keyData.Add(KeyKeys.KeyType.AsCBOR(), KeyKeys.KeyType_Octet);
         keyData.Add(KeyKeys.KeyId.AsCBOR(), 
                 "psk".getBytes(Constants.charset));
-        keyData.Add(KeyKeys.Octet_K.AsCBOR(), key128);
+        keyData.Add(KeyKeys.Octet_K.AsCBOR(), key128a);
         symmetricKey = new OneKey(keyData); 
         
         //Set up DTLSProfileTokenRepository
@@ -124,9 +129,12 @@ public class TestDTLSProfileAuthzInfo {
         Map<String, Map<String, Set<String>>> myScopes = new HashMap<>();
         myScopes.put("r_temp", myResource);
         
-        myResource.clear();
-        myResource.put("co2", actions);
-        myScopes.put("r_co2", myResource);
+        Set<String> actions2 = new HashSet<>();
+        actions.add("GET");
+        actions.add("POST");
+        Map<String, Set<String>> myResource2 = new HashMap<>();
+        myResource.put("co2", actions2);
+        myScopes.put("rw_co2", myResource2);
         
         KissValidator valid = new KissValidator(Collections.singleton("rs1"),
                 myScopes);
@@ -138,7 +146,7 @@ public class TestDTLSProfileAuthzInfo {
         //Set up COSE parameters
         COSEparams coseP = new COSEparams(MessageTag.Encrypt0, 
                 AlgorithmID.AES_CCM_16_128_128, AlgorithmID.Direct);
-        ctx = CwtCryptoCtx.encrypt0(key128, coseP.getAlg().AsCBOR());
+        ctx = CwtCryptoCtx.encrypt0(key128a, coseP.getAlg().AsCBOR());
         
         //Set up the inner Authz-Info library
         ai = new AuthzInfo(tr, Collections.singletonList("TestAS"), 
@@ -171,21 +179,36 @@ public class TestDTLSProfileAuthzInfo {
     }
     
     /**
-     * Test something FIXME
+     * Test a POST to /authz-info
      * @throws UnknownHostException 
+     * @throws AceException 
      */
     @Test
-    public void testBlah() throws UnknownHostException {
+    public void testBlah() throws UnknownHostException, AceException {
         Request req = new Request(Code.POST);
         req.setPayload(payload.EncodeToBytes());
         req.setDestination(InetAddress.getLocalHost());
         req.setSenderIdentity(new PreSharedKeyIdentity("psk"));
         req.setType(Type.NON);
         req.setAcknowledged(false);
+        req.setSource(InetAddress.getLocalHost());
+        req.setSourcePort(CoAP.DEFAULT_COAP_PORT);
+        req.setToken(new byte[]{0x01});
         Exchange iex = new Exchange(req, Origin.REMOTE);
-        iex.setRequest(req);
+        iex.setRequest(req);       
+        iex.setEndpoint(new CoapEndpoint());
         CoapExchange ex = new CoapExchange(iex, dai);      
         dai.handlePOST(ex);
+        
+        //Test that the PoP key was stored
+        Assert.assertArrayEquals(key128,
+                ai.getKey("ourKey").get(KeyKeys.Octet_K).GetByteString());
+        
+        
+       //Test that the token is there
+        Assert.assertEquals(TokenRepository.OK, 
+                tr.canAccess("ourKey", "ourKey", "temp", "GET", 
+                        new KissTime(), null));
     }
     
     
