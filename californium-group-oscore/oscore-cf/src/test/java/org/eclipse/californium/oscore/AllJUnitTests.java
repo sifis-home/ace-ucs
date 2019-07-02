f36a704ab9aa1318f729024da3e6ee6b16cac946
rikard-sics <rikard.hoglund@ri.se>
2019-07-02 18:04:25 +0200

    Small fix for OSCORE JUnit tests

--- californium-group-oscore/oscore-cf/src/test/java/org/eclipse/californium/oscore/AllJUnitTests.java
+++ californium-group-oscore/oscore-cf/src/test/java/org/eclipse/californium/oscore/AllJUnitTests.java
@@ -28,7 +28,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ ByteIdTest.class, HashMapCtxDBTest.class, OptionJuggleTest.class, OSCoreCtxTest.class,
 	OSCoreTest.class, OSSerializerTest.class, OSCoreServerClientTest.class,	DecryptorTest.class,
-	OSCoreMessageExchangeTest.class })
+	OSCoreMessageExchangeTest.class, EncryptorTest.class })
 public class AllJUnitTests {
 
 }
