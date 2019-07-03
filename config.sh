0c12f698369223becfd07e8618f3bcbc1d020632
rikard-sics <rikard.hoglund@ri.se>
2019-07-03 14:40:52 +0200

    Updated config script to warn when ace/db.pwd is missing

--- config.sh
+++ config.sh
@@ -12,3 +12,11 @@ cd joint-applications
 mvn eclipse:eclipse
 cd ..
 
+#Check that the ace/db.pwd file exists
+FILE=ace/db.pwd
+if [ ! -f "$FILE" ]; then
+ echo
+ echo "Warning: File ace/db.pwd is missing!"
+ echo
+fi
+
