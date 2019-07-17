2d94edbc47f65e8ef54d25d38900b0fd225942cc
rikard-sics <rikard.hoglund@ri.se>
2019-07-17 11:56:08 +0200

    Work on OSCORE Client-GM app. making it perform proper Join request

--- ace/src/test/java/se/sics/ace/oscore/group/OSCoreClientGroupOSCORE.java
+++ ace/src/test/java/se/sics/ace/oscore/group/OSCoreClientGroupOSCORE.java
@@ -1,28 +1,42 @@
 package se.sics.ace.oscore.group;
 
 import java.net.InetSocketAddress;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.xml.bind.DatatypeConverter;
 
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
 import org.eclipse.californium.core.coap.Request;
 import org.eclipse.californium.core.coap.Response;
+import org.eclipse.californium.core.coap.CoAP.Code;
+import org.eclipse.californium.core.coap.CoAP.Type;
+import org.eclipse.californium.oscore.GroupOSCoreCtx;
 import org.eclipse.californium.oscore.HashMapCtxDB;
 import org.eclipse.californium.oscore.OSCoreCoapStackFactory;
 import org.eclipse.californium.oscore.OSCoreCtx;
 import org.eclipse.californium.oscore.OSException;
 import org.eclipse.californium.oscore.Utility;
 import com.upokecenter.cbor.CBORObject;
+import com.upokecenter.cbor.CBORType;
 
 import org.eclipse.californium.cose.AlgorithmID;
+import org.eclipse.californium.cose.KeyKeys;
 import org.eclipse.californium.cose.MessageTag;
+import org.eclipse.californium.cose.OneKey;
+import org.eclipse.californium.elements.util.Bytes;
+
 import se.sics.ace.COSEparams;
 import se.sics.ace.Constants;
 import se.sics.ace.coap.client.OSCOREProfileRequests;
 import se.sics.ace.cwt.CWT;
 import se.sics.ace.cwt.CwtCryptoCtx;
+import se.sics.ace.oscore.GroupOSCORESecurityContextObject;
+import se.sics.ace.oscore.GroupOSCORESecurityContextObjectParameters;
 
 /**
  * A test case for the OSCORE profile interactions between client and server.
@@ -47,6 +61,13 @@ public class OSCoreClientGroupOSCORE {
 	private static String GM_HOST;
 	//private static String REQUESTED_RESOURCE = "helloWorld";
 	private static String REQUESTED_RESOURCE = "feedca570000";
+	
+	//Use DTLS or not (set it to false always)
+	private static boolean useDTLS = false;
+
+	// Private and public key to be used in the OSCORE group (EDDSA)
+    private static String groupKeyPair = "pQMnAQEgBiFYIAaekSuDljrMWUG2NUaGfewQbluQUfLuFPO8XMlhrNQ6I1ggZHFNQaJAth2NgjUCcXqwiMn0r2/JhEVT5K1MQsxzUjk=";
+
 	
     /**
      * The cnf key used in these tests
@@ -135,15 +156,224 @@ public class OSCoreClientGroupOSCORE {
         OSCoreCtx generatedContext = HashMapCtxDB.getInstance().getContext(resourceURI);
         Utility.printContextInfo(generatedContext);
         
-		//Submit a request to GM
-		System.out.println("Performing request to GM at " + resourceURI + " (port " + GM_PORT + ")");
+		//Submit a request to GM (normal request would be done here)
+//		System.out.println("Performing request to GM at " + resourceURI + " (port " + GM_PORT + ")");
 		CoapClient c = OSCOREProfileRequests.getClient(new InetSocketAddress(
 				resourceURI, GM_PORT));
-		Request helloReq = new Request(CoAP.Code.POST);
-		helloReq.setPayload("HELLO");
-		helloReq.getOptions().setOscore(new byte[0]);
-		CoapResponse helloRes = c.advanced(helloReq);
-		System.out.println("Received response from GM:" + helloRes.getResponseText());
+//		Request helloReq = new Request(CoAP.Code.POST);
+//		helloReq.setPayload("HELLO");
+//		helloReq.getOptions().setOscore(new byte[0]);
+//		CoapResponse helloRes = c.advanced(helloReq);
+//		System.out.println("Received response from GM:" + helloRes.getResponseText());
+		//Note: Re-enable these 8 lines above to have the normal request functionality
+		
+		
+		/* Create and perform Join Request below */
+		
+		//First set up some parameters
+		boolean askForPubKeys = true;
+    	boolean providePublicKey = true;
+    	
+        String gid = new String("feedca570000");
+     	String role1 = new String("requester");
+     	String role2 = new String("listener");
+    	
+        CBORObject cborArrayScope = CBORObject.NewArray();
+    	cborArrayScope.Add(gid);
+    	CBORObject cborArrayRoles = CBORObject.NewArray();
+    	cborArrayRoles.Add(role1);
+    	cborArrayRoles.Add(role2);
+    	cborArrayScope.Add(cborArrayRoles);
+    	byte[] byteStringScope = cborArrayScope.EncodeToBytes();
+    	
+    	//Now proceed with creating Join request
+		
+		System.out.println("Performing Join request using OSCORE to GM at " + resourceURI);
+        c.setURI(resourceURI);
+        
+        CBORObject requestPayload = CBORObject.NewMap();
+        
+        requestPayload.Add("scope", CBORObject.FromObject(byteStringScope));
+        
+        if (askForPubKeys) {
+        	
+        	CBORObject getPubKeys = CBORObject.NewArray();
+        	requestPayload.Add("get_pub_keys", getPubKeys);
+        	
+        }
+        
+        if (providePublicKey) {
+        	
+        	// For the time being, the client's public key can be only a COSE Key
+        	OneKey publicKey = new OneKey(CBORObject.DecodeFromBytes(Base64.getDecoder().decode(groupKeyPair))).PublicKey();
+        	requestPayload.Add("client_cred", publicKey.AsCBOR().EncodeToBytes());
+        	
+        }
+        
+        Request joinReq = new Request(Code.POST, Type.CON);
+        joinReq.getOptions().setOscore(new byte[0]);
+        joinReq.setPayload(requestPayload.EncodeToBytes());
+        joinReq.getOptions().setContentFormat(MediaTypeRegistry.APPLICATION_CBOR);
+        if(!useDTLS) {
+        	joinReq.getOptions().setOscore(Bytes.EMPTY); //Enable OSCORE for Join request
+        }
+        
+        //Transmit the Join request
+        
+        System.out.println("");
+        System.out.println("Sent Join request to GM: " + requestPayload.toString());
+        CoapResponse r2 = c.advanced(joinReq);
+        
+        byte[] responsePayload = r2.getPayload();
+
+        //System.out.println("Received response to Join req from GM: " + r2.getResponseText());
+        
+        CBORObject joinResponse = CBORObject.DecodeFromBytes(responsePayload);
+
+        CBORObject myMap = joinResponse.get("key");
+        
+        if(myMap.size() != 9) {
+        	System.out.println("Received bad response from GM: " + r2.getResponseText());
+        } else {
+        	System.out.println("Received Join response from GM: " + joinResponse.toString());
+        }
+
+        // Add default values for missing parameters
+        if (myMap.ContainsKey(CBORObject.FromObject(GroupOSCORESecurityContextObjectParameters.hkdf)) == false)
+        	myMap.Add(GroupOSCORESecurityContextObjectParameters.hkdf, AlgorithmID.HKDF_HMAC_SHA_256);
+        if (myMap.ContainsKey(CBORObject.FromObject(GroupOSCORESecurityContextObjectParameters.alg)) == false)
+        	myMap.Add(GroupOSCORESecurityContextObjectParameters.alg, AlgorithmID.AES_CCM_16_64_128);
+        if (myMap.ContainsKey(CBORObject.FromObject(GroupOSCORESecurityContextObjectParameters.salt)) == false)
+        	myMap.Add(GroupOSCORESecurityContextObjectParameters.salt, CBORObject.FromObject(new byte[0]));
+        if (myMap.ContainsKey(CBORObject.FromObject(GroupOSCORESecurityContextObjectParameters.rpl)) == false)
+        	myMap.Add(GroupOSCORESecurityContextObjectParameters.rpl, CBORObject.FromObject((int)32));        
+        
+        Map<Short, CBORObject> contextParams = new HashMap<>(GroupOSCORESecurityContextObjectParameters.getParams(myMap));
+        GroupOSCORESecurityContextObject contextObject = new GroupOSCORESecurityContextObject(contextParams); 
+        
+        CBORObject coseKeySetArray = null;
+        if (askForPubKeys) {
+        	// The content of the byte string should be a COSE_KeySet, to be processed accordingly
+        	
+        	byte[] coseKeySetByte = joinResponse.get("pub_keys").GetByteString();
+        	coseKeySetArray = CBORObject.DecodeFromBytes(coseKeySetByte); 	
+        }
+        else {
+        	System.err.println("Join response did not contain pub_keys!");
+        }
+        
+        
+        /* Group OSCORE Context derivation below */
+        
+        //Defining variables to hold the information before derivation
+        
+        //Algorithm
+        AlgorithmID algo = null;
+        CBORObject alg_param = contextObject.getParam(GroupOSCORESecurityContextObjectParameters.alg);
+        if(alg_param.getType() == CBORType.TextString) {
+        	algo = AlgorithmID.valueOf(alg_param.AsString());
+        } else if(alg_param.getType() == CBORType.SimpleValue) {
+        	algo = AlgorithmID.FromCBOR(alg_param);
+        }
+        
+        //KDF
+        AlgorithmID kdf = null;
+        CBORObject kdf_param = contextObject.getParam(GroupOSCORESecurityContextObjectParameters.hkdf);
+        if(kdf_param.getType() == CBORType.TextString) {
+        	kdf = AlgorithmID.valueOf(kdf_param.AsString());
+        } else if(kdf_param.getType() == CBORType.SimpleValue) {
+        	kdf = AlgorithmID.FromCBOR(kdf_param);
+        }
+        
+    	//Algorithm for the countersignature
+        AlgorithmID alg_countersign = null;
+        CBORObject alg_countersign_param = contextObject.getParam(GroupOSCORESecurityContextObjectParameters.cs_alg);
+        if(alg_countersign_param.getType() == CBORType.TextString) {
+        	alg_countersign = AlgorithmID.valueOf(alg_countersign_param.AsString());
+        } else if(alg_countersign_param.getType() == CBORType.SimpleValue) {
+        	alg_countersign = AlgorithmID.FromCBOR(alg_countersign_param);
+        }
+        
+        //Parameter for the countersignature
+        Integer par_countersign = null;
+        CBORObject par_countersign_param = contextObject.getParam(GroupOSCORESecurityContextObjectParameters.cs_params);
+        if(par_countersign_param.getType() == CBORType.Map) {
+        	par_countersign = par_countersign_param.get(KeyKeys.OKP_Curve.name()).AsInt32();
+        } else {
+        	System.err.println("Unknown par_countersign value!");
+        }
+    
+    	//Master secret
+    	CBORObject master_secret_param = contextObject.getParam(GroupOSCORESecurityContextObjectParameters.ms);
+    	byte[] master_secret = null;
+    	if(master_secret_param.getType() == CBORType.ByteString) {
+    		master_secret = master_secret_param.GetByteString();
+   		}
+    	
+    	//Master salt
+    	CBORObject master_salt_param = contextObject.getParam(GroupOSCORESecurityContextObjectParameters.salt);
+    	byte[] master_salt = null;
+    	if(master_salt_param.getType() == CBORType.ByteString) {
+    		master_salt = master_salt_param.GetByteString();
+   		}
+ 
+    	//Sender ID
+    	byte[] sid = null;
+    	CBORObject sid_param = contextObject.getParam(GroupOSCORESecurityContextObjectParameters.clientId);
+    	if(sid_param.getType() == CBORType.ByteString) {
+    		sid = sid_param.GetByteString();
+    	}
+    	
+    	//Group ID / Context ID
+    	CBORObject group_identifier_param = contextObject.getParam(GroupOSCORESecurityContextObjectParameters.contextId);
+    	byte[] group_identifier = null;
+    	if(group_identifier_param.getType() == CBORType.ByteString) {
+    		group_identifier = group_identifier_param.GetByteString();
+    	}
+    	
+    	//RPL (replay window information)
+    	CBORObject rpl_param = contextObject.getParam(GroupOSCORESecurityContextObjectParameters.rpl);
+    	int rpl = 32; //Default value
+    	if(rpl_param != null && rpl_param.getType() == CBORType.SimpleValue) {
+    		rpl = rpl_param.AsInt32();
+    	}
+    	
+    	//Set up private & public keys for sender (not from response but set by client)
+    	String sid_private_key_string = groupKeyPair;
+    	OneKey sid_private_key;
+       	sid_private_key = new OneKey(CBORObject.DecodeFromBytes(DatatypeConverter.parseBase64Binary(sid_private_key_string)));
+
+    	//Now derive the actual context
+    	
+    	GroupOSCoreCtx oscoreCtx = null;
+		try {
+			oscoreCtx = new GroupOSCoreCtx(master_secret, true, algo, sid, kdf, rpl, 
+					master_salt, group_identifier, alg_countersign, par_countersign, sid_private_key);
+		} catch (OSException e) {
+			System.err.println("Failed to derive Group OSCORE Context!");
+			e.printStackTrace();
+		}
+		
+		//Finally add the recipient contexts from the coseKeySetArray
+		for(int i = 0 ; i < coseKeySetArray.size() ; i++) {
+			
+			CBORObject key_param = coseKeySetArray.get(i);
+			
+	    	byte[] rid = null;
+	    	CBORObject rid_param = key_param.get(KeyKeys.KeyId.AsCBOR());
+	    	if(rid_param.getType() == CBORType.ByteString) {
+	    		rid = rid_param.GetByteString();
+	    	}
+			
+			OneKey recipient_key = new OneKey(key_param);
+			
+			oscoreCtx.addRecipientContext(rid, recipient_key);
+		}
+		
+		//Print information about the created context
+		System.out.println();
+		System.out.println("Generated Group OSCORE Context from received information:");
+		Utility.printContextInfo(oscoreCtx);
 
        
     }
