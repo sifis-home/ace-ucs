059ddd89999b2ba82bbd4517662dd8abf206792d
marco-tiloca-sics <marco.tiloca@ri.se>
2020-02-28 18:17:09 +0100

    Minor.

--- src/main/java/se/sics/ace/oscore/GroupOSCORESecurityContextObjectParameters.java
+++ src/main/java/se/sics/ace/oscore/GroupOSCORESecurityContextObjectParameters.java
@@ -43,25 +43,25 @@ package se.sics.ace.oscore;
 		/**
 		 * 'cs_alg' - Group OSCORE Countersignature Algorithm value
 		 */
-	    // Assume that "cs_alg" is registered with label 9 in the "OSCORE Security Context Parameters" Registry of draft-ietf-ace-oscore-profile
+	    // Assume that "cs_alg" is registered with label 8 in the "OSCORE Security Context Parameters" Registry of draft-ietf-ace-oscore-profile
 		public static final short cs_alg = 8; // Major type 0 (unsigned integer) or 1 (negative integer) or 3 (text string)
 		
 		/**
 		 *  'cs_params' - Group OSCORE Countersignature algorithm Parameter Value
 		 */
-		// Assume that "cs_params" is registered with label 10 in the "OSCORE Security Context Parameters" Registry of draft-ietf-ace-oscore-profile
+		// Assume that "cs_params" is registered with label 9 in the "OSCORE Security Context Parameters" Registry of draft-ietf-ace-oscore-profile
 		public static final short cs_params = 9; // Major type 5 (map)
 		
 		/**
 		 *  'cs_key_params' - Group OSCORE Countersignature algorithm Key Parameter Value
 		 */
-		// Assume that "cs_key_params" is registered with label 11 in the "OSCORE Security Context Parameters" Registry of draft-ietf-ace-oscore-profile
+		// Assume that "cs_key_params" is registered with label 10 in the "OSCORE Security Context Parameters" Registry of draft-ietf-ace-oscore-profile
 		public static final short cs_key_params = 10; // Major type 5 (map)
 		
 		/**
 		 *  'cs_key_enc' - Group OSCORE Countersignature Key Encoding Value
 		 */
-		// Assume that "cs_key_enc" is registered with label 12 in the "OSCORE Security Context Parameters" Registry of draft-ietf-ace-oscore-profile
+		// Assume that "cs_key_enc" is registered with label 11 in the "OSCORE Security Context Parameters" Registry of draft-ietf-ace-oscore-profile
 		public static final short cs_key_enc = 11; // Major type 0 (unsigned integer) or 1 (negative integer)
 		
 		/**
