19fa3b8db04d1de18b6b5b7e214a0a523939fa8e
marco-tiloca-sics <marco.tiloca@ri.se>
2021-01-06 17:32:51 +0100

    Group OSCORE discovery: interactions with the Resource Directory

--- src/test/java/se/sics/ace/interopGroupOSCORE/CoAPEndpointToResourceDirectory.java
+++ src/test/java/se/sics/ace/interopGroupOSCORE/CoAPEndpointToResourceDirectory.java
@@ -26,13 +26,18 @@ import java.net.URISyntaxException;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.Utils;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
 import org.eclipse.californium.elements.exception.ConnectorException;
+
+import se.sics.ace.Constants;
 
 import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.core.network.config.NetworkConfigDefaultHandler;
 import org.eclipse.californium.core.network.config.NetworkConfig.Keys;
 
 public class CoAPEndpointToResourceDirectory {
+	
+	private static boolean debug = true;
 	
 	private static final File CONFIG_FILE = new File("Californium.properties");
 	private static final String CONFIG_HEADER = "Californium CoAP Properties file for Fileclient";
@@ -50,6 +55,32 @@ public class CoAPEndpointToResourceDirectory {
 		}
 	};
 
+	private static final String endpointName = "gm1";
+	private static final String securityGroupName = "feedca570000";
+	private static final String applicationGroupName = "group1";
+	private static final String multicastAddress = "[ff35:30:2001:db8:f1::8000:1]";
+	
+	private static final String securityGroupRegistrationRequest = "" +
+			/* Beginning of Link-Format payload */
+	        "</ace-group/feedca570000>;ct=41;rt=\"core.osc.gm\";if=\"ace.group\";" + 
+				"sec-gp=\"" + securityGroupName + "\";app-gp=\"" + applicationGroupName + "\";" + 
+				"cs_alg=\"-8\";cs_alg_crv=\"6\";" + 
+				"cs_key_kty=\"1\";cs_key_crv=\"6\";" + 
+				"cs_kenc=\"1\";ecdh_alg=\"-27\";" + 
+				"ecdh_alg_crv=\"4\";ecdh_key_kty=\"1\";" + 
+				"ecdh_key_crv=\"4\"," + 
+			"<coap://as.example.com/token>;" + 
+				"rel=\"authorization-server\";" + 
+				"anchor=\"coap://[2001:db8::ab]/ace-group/feedca570000\"" +
+			/* End of Link-Format payload */
+			"";
+
+	private static final String applicationGroupRegistrationRequest = "" + 
+			/* Beginning of Link-Format payload */
+			"</light>;rt=\"tag:example.com,2020:light\";if=\"tag:example.net,2020:actuator\"" +
+			/* End of Link-Format payload */
+			"";
+	
 	/*
 	 * Application entry point.
 	 * 
@@ -60,6 +91,7 @@ public class CoAPEndpointToResourceDirectory {
 
 		String path = "";
 		String query = "";
+		byte[] requestPayload = null;
 		
 		NetworkConfig config = NetworkConfig.createWithFile(CONFIG_FILE, CONFIG_HEADER, DEFAULTS);
 		NetworkConfig.setStandard(config);
@@ -83,26 +115,69 @@ public class CoAPEndpointToResourceDirectory {
 		// Interact with the Resource Directory
 		
 		// Test reachability
+		System.out.println("\n\n=============\nGET request to well-known/core");
 		path = "/.well-known/core";
 		uri = buildURI(resourceDirectoryURI + path);
 		getRequestToResourceDirectory(args, uri);
 		
-		// Dummy resource discovery, a 4.04 response is expected
+		// Dummy resource lookup, a 4.04 response is expected
+		/*
+		System.out.println("\n\n=============Dummy resource lookup\n\n");
 		path = "/resource-lookup";
 		query = "?rt=light";
 		uri = buildURI(resourceDirectoryURI + path + query);
-		getRequestToResourceDirectory(args, uri);
+		if (debug) {
+			System.out.println("\nURI: " + resourceDirectoryURI + path + query);
+		}
+		// getRequestToResourceDirectory(args, uri);
+		*/
+		
 		
 		// Register an application group
-		// postRequestToResourceDirectory(args, uri);
+		System.out.println("\n\n=============\nRegister an application group");
+		path = "/rd";
+		query = "?ep=" + applicationGroupName + "&et=core.rd-group&base=coap://" + multicastAddress;
+		uri = buildURI(resourceDirectoryURI + path + query);
+		requestPayload = applicationGroupRegistrationRequest.getBytes(Constants.charset);
+		if (debug) {
+			System.out.println("\nURI: " + resourceDirectoryURI + path + query +
+					           "\nPayload: " + new String(requestPayload, Constants.charset) + "\n");
+		}
+		//postRequestToResourceDirectory(args, uri, requestPayload);
+		
 
 		// Register a security group
-		// postRequestToResourceDirectory(args, uri);
+		System.out.println("\n\n=============\nRegister a security group");
+		path = "/rd";
+		query = "?ep=" + endpointName;
+		uri = buildURI(resourceDirectoryURI + path + query);
+		requestPayload = securityGroupRegistrationRequest.getBytes(Constants.charset);
+		if (debug) {
+			System.out.println("\nURI: " + resourceDirectoryURI + path + query +
+			                   "\nPayload: " + new String(requestPayload, Constants.charset) + "\n");
+		}
+		postRequestToResourceDirectory(args, uri, requestPayload);
+		
 		
 		// Discover the security group(s)
-		// getRequestToResourceDirectory(args, uri);
+		System.out.println("\n\n=============\nDiscover the security group(s)");
+		path = "/resource-lookup";
+		query = "?rt=core.osc.gm&app-gp=" + applicationGroupName;
+		uri = buildURI(resourceDirectoryURI + path + query);
+		if (debug) {
+			System.out.println("\nURI: " + resourceDirectoryURI + path + query + "\n");
+		}
+		getRequestToResourceDirectory(args, uri);
 		
-		// Discover the application group(s)
+		
+		// Discover the multicast address of the application group
+		System.out.println("\n\n=============\nDiscover the multicast address of the application group");
+		path = "/endpoint-lookup";
+		query = "?et=core.rd-group&ep=" + applicationGroupName;
+		uri = buildURI(resourceDirectoryURI + path + query);
+		if (debug) {
+			System.out.println("\nURI: " + resourceDirectoryURI + path + query + "\n");
+		}
 		// getRequestToResourceDirectory(args, uri);
 
 	}
@@ -150,8 +225,45 @@ public class CoAPEndpointToResourceDirectory {
 
 		CoapResponse response = null;
 		try {
-			System.out.println("\n\nSending request to: " + targetUri + "\n");
+			System.out.println("\n\nSending GET request to: " + targetUri + "\n");
 			response = client.get();
+		} catch (ConnectorException | IOException e) {
+			System.err.println("Got an error: " + e);
+		}
+
+		if (response != null) {
+
+			System.out.println(response.getCode());
+			System.out.println(response.getOptions());
+			if (args.length > 1) {
+				try (FileOutputStream out = new FileOutputStream(args[1])) {
+					out.write(response.getPayload());
+				} catch (IOException e) {
+					System.err.println("Error while writing the response payload to file: " +  e.getMessage());
+				}
+			} else {
+				System.out.println(response.getResponseText());
+
+				System.out.println(System.lineSeparator() + "ADVANCED" + System.lineSeparator());
+				// access advanced API with access to more details through
+				// .advanced()
+				System.out.println(Utils.prettyPrint(response));
+			}
+		} else {
+			System.out.println("No response received.");
+		}
+		client.shutdown();
+		
+	}
+	
+	private static void postRequestToResourceDirectory(final String args[], final URI targetUri, final byte[] payload) {
+		
+		CoapClient client = new CoapClient(targetUri);
+
+		CoapResponse response = null;
+		try {
+			System.out.println("\n\nSending POST request to: " + targetUri + "\n");
+			response = client.post(payload, MediaTypeRegistry.APPLICATION_LINK_FORMAT);
 		} catch (ConnectorException | IOException e) {
 			System.err.println("Got an error: " + e);
 		}
