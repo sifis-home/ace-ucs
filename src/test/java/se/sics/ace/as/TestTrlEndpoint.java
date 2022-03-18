461383f2b38ff7e5bda041205f9b7c3e007550bf
marco-rasori <marco.rasori@gmail.com>
2022-03-18 11:23:27 +0100

    Added more tests, mainly for query parameters

--- src/test/java/se/sics/ace/as/TestTrlEndpoint.java
+++ src/test/java/se/sics/ace/as/TestTrlEndpoint.java
@@ -3,6 +3,7 @@ package se.sics.ace.as;
 import COSE.CoseException;
 import COSE.OneKey;
 import com.upokecenter.cbor.CBORObject;
+import com.upokecenter.cbor.CBORType;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -51,6 +52,11 @@ public class TestTrlEndpoint {
         peerIdentitiesToNames.put("Id2", "clientB");
         peerIdentitiesToNames.put("Id3", "rs1");
         peerIdentitiesToNames.put("Id4", "rs2");
+        db.addClient("clientA",new HashSet<String>(){{add("coap_dtls");}},
+                null,null,new HashSet<String>(){{add("PSK");}},new OneKey(),null);
+        db.addClient("clientB",new HashSet<String>(){{add("coap_dtls");}},
+                null,null,new HashSet<String>(){{add("PSK");}},new OneKey(),null);
+
 
         db.addCti2Peers("cti1", "clientB", new HashSet<String>() {{
             add("rs1");
@@ -88,6 +94,10 @@ public class TestTrlEndpoint {
     @Test
     public void testUnauthorizedClient() throws Exception {
         Message msg = new LocalMessage(-1, "Id5", null, (CBORObject) null);
+
+        r.setQueryParameters(new HashMap<>());
+        r.setHasObserve(false);
+
         Message response = r.processMessage(msg);
 
         assert (response.getMessageCode() == Message.FAIL_UNAUTHORIZED);
@@ -109,6 +119,10 @@ public class TestTrlEndpoint {
     @Test
     public void testSucceedNoHashes() throws Exception {
         Message msg = new LocalMessage(-1, "Id4", null, (CBORObject) null);
+
+        r.setQueryParameters(new HashMap<>());
+        r.setHasObserve(false);
+
         Message response = r.processMessage(msg);
 
         assert (response.getMessageCode() == Message.CONTENT);
@@ -128,6 +142,10 @@ public class TestTrlEndpoint {
     @Test
     public void testSucceed() throws Exception {
         Message msg = new LocalMessage(-1, "Id2", null, (CBORObject) null);
+
+        r.setQueryParameters(new HashMap<>());
+        r.setHasObserve(false);
+
         Message response = r.processMessage(msg);
 
         assert (response.getMessageCode() == Message.CONTENT);
@@ -140,4 +158,135 @@ public class TestTrlEndpoint {
 
     }
 
+
+    /**
+     * Test the parsing of query parameters at the trl endpoint
+     * In all these cases, the test should return a 'invalid parameter value' error.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testInvalidParameterValue() throws Exception {
+        Message msg = new LocalMessage(-1, "Id2", null, (CBORObject) null);
+
+        // 'diff' lower than 0
+        Map<String,Integer> qParams = new HashMap<>();
+        qParams.put("diff", -3);
+        r.setQueryParameters(qParams);
+        r.setHasObserve(false);
+
+        Message response = r.processMessage(msg);
+
+        CBORObject payload = CBORObject.DecodeFromBytes(response.getRawPayload());
+        Map<Short, CBORObject> map = Constants.getParams(payload);
+        assert(map.containsKey(Constants.TRL_ERROR));
+        Assert.assertEquals(map.get(Constants.TRL_ERROR).AsNumber().ToInt16Checked(), Constants.INVALID_PARAMETER_VALUE);
+
+
+        // 'pmax' equal to 0 with observe option set
+        msg = new LocalMessage(-1, "Id2", null, (CBORObject) null);
+
+        qParams = new HashMap<>();
+        qParams.put("pmax", 0);
+        r.setQueryParameters(qParams);
+        r.setHasObserve(true);
+
+        response = r.processMessage(msg);
+
+        payload = CBORObject.DecodeFromBytes(response.getRawPayload());
+        map = Constants.getParams(payload);
+        assert(map.containsKey(Constants.TRL_ERROR));
+        Assert.assertEquals(map.get(Constants.TRL_ERROR).AsNumber().ToInt16Checked(), Constants.INVALID_PARAMETER_VALUE);
+
+
+        // 'pmax' lower than 0 with observe option set
+        msg = new LocalMessage(-1, "Id2", null, (CBORObject) null);
+
+        qParams = new HashMap<>();
+        qParams.put("pmax", 0);
+        r.setQueryParameters(qParams);
+        r.setHasObserve(true);
+
+        response = r.processMessage(msg);
+
+        payload = CBORObject.DecodeFromBytes(response.getRawPayload());
+        map = Constants.getParams(payload);
+        assert(map.containsKey(Constants.TRL_ERROR));
+        Assert.assertEquals(map.get(Constants.TRL_ERROR).AsNumber().ToInt16Checked(), Constants.INVALID_PARAMETER_VALUE);
+
+
+        // 'cursor' lower than 0
+        msg = new LocalMessage(-1, "Id2", null, (CBORObject) null);
+
+        qParams = new HashMap<>();
+        qParams.put("diff", 1);
+        qParams.put("cursor", -1);
+        r.setQueryParameters(qParams);
+        r.setHasObserve(false);
+
+        response = r.processMessage(msg);
+
+        payload = CBORObject.DecodeFromBytes(response.getRawPayload());
+        map = Constants.getParams(payload);
+        assert(map.containsKey(Constants.TRL_ERROR));
+        Assert.assertEquals(map.get(Constants.TRL_ERROR).AsNumber().ToInt16Checked(), Constants.INVALID_PARAMETER_VALUE);
+    }
+
+    /**
+     * Test the parsing of query parameters at the trl endpoint
+     * In all these cases, the test should return a 'invalid set of parameters' error.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testInvalidSetOfParameters() throws Exception {
+
+        // 'cursor' present and 'diff' not present
+        Message msg = new LocalMessage(-1, "Id2", null, (CBORObject) null);
+
+        Map<String, Integer> qParams = new HashMap<>();
+        qParams.put("cursor", 1);
+        r.setQueryParameters(qParams);
+        r.setHasObserve(false);
+
+        Message response = r.processMessage(msg);
+
+        CBORObject payload = CBORObject.DecodeFromBytes(response.getRawPayload());
+        Map<Short, CBORObject> map = Constants.getParams(payload);
+        assert (map.containsKey(Constants.TRL_ERROR));
+        Assert.assertEquals(map.get(Constants.TRL_ERROR).AsNumber().ToInt16Checked(), Constants.INVALID_SET_OF_PARAMETERS);
+    }
+
+    /**
+     * Test the parsing of query parameters at the trl endpoint
+     * In all these cases, the test should return a 'invalid set of parameters' error.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testIgnorePmaxIfObserveNotSet() throws Exception {
+
+        // valid 'pmax' with observe option not set
+        Message msg = new LocalMessage(-1, "Id2", null, (CBORObject) null);
+
+        Map<String, Integer> qParams = new HashMap<>();
+        qParams.put("pmax", 1);
+        r.setQueryParameters(qParams);
+        r.setHasObserve(false);
+
+        Message response = r.processMessage(msg);
+
+        // if the trl responds with a CBOR map, check that the map does not contain an error.
+        // The trl can use a CBOR map for a successful message when the "third mode" --the
+        // one that uses 'cursor'-- is used.
+        CBORObject payload = CBORObject.DecodeFromBytes(response.getRawPayload());
+        if (payload.getType() == CBORType.Map) {
+            Map<Short, CBORObject> map = Constants.getParams(payload);
+            assert (!map.containsKey(Constants.TRL_ERROR));
+        }
+        // otherwise, the trl responds with a CBOR array
+        else
+            assert(payload.getType() == CBORType.Array);
+
+    }
 }
